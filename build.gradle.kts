250d94511fd438a3680289b9e19a35192f2827a6
Frolov.Va.A <frolov.va.a1@sblogistica.ru>
2025-09-30 16:58:46 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.2.8"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.frolov"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/frolov/HolisticHealthDashboardApplication.java
@@ -0,0 +1,11 @@
+package frolov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolisticHealthDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HolisticHealthDashboardApplication.class, args);
+    }
+}
