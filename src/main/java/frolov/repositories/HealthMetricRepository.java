81aceb8730474860ef714622e103e99762ec9f4a
Frolov.Va.A <frolov.va.a1@sblogistica.ru>
2025-10-09 18:19:21 +0300

    add HealthMetricService

---
+++ src/main/java/frolov/repositories/HealthMetricRepository.java
@@ -0,0 +1,10 @@
+package frolov.repositories;
+
+import frolov.models.HealthMetric;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HealthMetricRepository extends JpaRepository<HealthMetric, Long> {
+
+}
